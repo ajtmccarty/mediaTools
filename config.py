3f71a69775645f38567c1297342ef86fc5904bc8
AJTM <aaronjohntmccarty@gmail.com>
2016-06-22 20:23:01 +0000

    Add type mapping to config for converting Python type to SQL type

--- config.py
+++ config.py
@@ -1,4 +1,6 @@
-MOVIE_TITLES_FILE = "./AllMovies.txt"
+import datetime
+
+MOVIE_TITLES_FILE = "./Movies/"
 MOVIE_FILE_EXTENSIONS = ['avi','mov','wmv','mp4','m4p','m4v','mpg','mpeg', \
                          'mpe','mpv']
 DB_SETTINGS = {
@@ -6,4 +8,18 @@ DB_SETTINGS = {
   "db_name": "movies",
   "username": "ubuntu",
   "password": "foobar"
+}
+TYPE_MAPPING = {
+  type(None):'NULL',
+  bool:'bool',
+  float:['real','double'],
+  int:['smallint','integer','bigint'],
+  long:['smallint','integer','bigint'],
+  str:['varchar','text'],
+  unicode:['varchar','text'],
+  datetime.date:['date'],
+  datetime.time:['time','timetz'],
+  datetime.datetime:['timestamp','timestamptz'],
+  datetime.timedelta:'interval',
+  list:'ARRAY',
 }
